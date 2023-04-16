f91fa5b374941a5a7d0a3f3bdded06c829376a23
Fl3r0n <romanovskymv@gmail.com>
2023-04-16 18:54:25 +0300

    Изменена система ввода аргументов, добавлена коммнда для удаления замены

--- main.py
+++ main.py
@@ -18,17 +18,22 @@ logger = logging.getLogger(__name__)
 '''
 
 
-async def les(update, conоtext):
+async def les(update, context):
+    context.args = ' '.join(update.message.text.split()[1:]).split('/')
     con = sqlite3.connect(DBNAME)
     td = date.today()
+    print(type(td))
+    td = datetime.strptime("2023-04-18", '%Y-%m-%d').date()
+    print(td)
     user_id = str(update.message.from_user.id)
     clas = con.cursor().execute("""Select class From User where telid = ?""", (user_id,)).fetchall()
     rep = con.cursor().execute("""Select numLesson, lessonRep, room From Replace where classRep = ? and day = ?""",
                                (clas[0][0], str(td),)).fetchall()
-    tdweek = datetime.today().weekday()
+    tdweek = td.weekday()
+    print(tdweek)
     lessons = con.cursor().execute(
         """Select lesson, room From Schedule where class = ? and dayOfWeek = ? order by numLesson""",
-        (clas[0][0], tdweek,)).fetchall()
+        (clas[0][0], tdweek + 1,)).fetchall()
     schedule = []
     for elem in lessons:
         schedule.append(str(elem[0]) + ' ' + str(elem[1]))
@@ -38,10 +43,14 @@ async def les(update, conоtext):
     if resstr:
         await update.message.reply_text('У Вас сегодня:')
         await update.message.reply_text(resstr)
+    else:
+        await update.message.reply_text('Сегодня у вас нет уроков!')
 
 
 async def reg(update, context):
+    context.args = ' '.join(update.message.text.split()[1:]).split('/')
     con = sqlite3.connect(DBNAME)
+    context.args = context.args.split('/')
     user_id = str(update.message.from_user.id)
     code = str(context.args[0])
     print(code)
@@ -69,6 +78,7 @@ async def send(chat, msg):
 
 
 async def announce_cl(update, context):
+    context.args = ' '.join(update.message.text.split()[1:]).split('/')
     loop = asyncio.get_event_loop()
     con = sqlite3.connect(DBNAME)
     user_id = str(update.message.from_user.id)
@@ -97,6 +107,7 @@ async def announce_cl(update, context):
 
 
 async def announce_st(update, context):
+    context.args = ' '.join(update.message.text.split()[1:]).split('/')
     loop = asyncio.get_event_loop()
     con = sqlite3.connect(DBNAME)
     user_id = str(update.message.from_user.id)
@@ -115,7 +126,8 @@ async def announce_st(update, context):
                         i = list(i)[0]
                         if str(i) != str(user_id):
                             print('sent')
-                            loop.create_task((send(int(i), str('Тебе пришло сообщение от ' + user_name + ': ' + message))))
+                            loop.create_task(
+                                (send(int(i), str('Тебе пришло сообщение от ' + user_name + ': ' + message))))
             else:
                 await update.message.reply_text('Некорректное имя!')
         else:
@@ -124,11 +136,12 @@ async def announce_st(update, context):
         await update.message.reply_text('У вас нет прав на использование этой команды!')
 
 
-async def addrep(update, context):
+async def add_rep(update, context):
+    context.args = ' '.join(update.message.text.split()[1:]).split('/')
     con: Connection = sqlite3.connect(DBNAME)
     user_id = str(update.message.from_user.id)
     stat = con.cursor().execute("""Select status From User where telid = ?""", (user_id,)).fetchone()
-    print(stat)
+    print(context.args)
     if stat == ('учитель',):
         rep = context.args
         rep[0] = "2023-" + rep[0]
@@ -138,13 +151,33 @@ async def addrep(update, context):
         con.commit()
 
 
+async def remove_rep(update, context):
+    context.args = ' '.join(update.message.text.split()[1:]).split('/')
+    con: Connection = sqlite3.connect(DBNAME)
+    user_id = str(update.message.from_user.id)
+    stat = con.cursor().execute("""Select status From User where telid = ?""", (user_id,)).fetchone()
+    print(context.args)
+    if stat == ('учитель',):
+        rep = context.args
+        rep[0] = "2023-" + rep[0]
+        print((rep[0], rep[1], str(rep[2])))
+        con.cursor().execute(
+            """DELETE FROM Replace WHERE day = ? and classRep = ? and numLesson = ? """,
+            (rep[0], rep[1], str(int(rep[2]) - 1)))
+        con.commit()
+        await update.message.reply_text('Замена успешно удалена!')
+    else:
+        await update.message.reply_text('У вас нет прав на использование этой команды!')
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("reg", reg))
     application.add_handler(CommandHandler("les", les))
     application.add_handler(CommandHandler("announce_cl", announce_cl))
     application.add_handler(CommandHandler("announce_st", announce_st))
-    application.add_handler(CommandHandler("addrep", addrep))
+    application.add_handler(CommandHandler("add_rep", add_rep))
+    application.add_handler(CommandHandler("remove_rep", remove_rep))
     application.run_polling()
     asyncio.run(send(1156166555, 'Hello there!'))
 
